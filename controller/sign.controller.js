0089f3da88be4f6686750384f75d9ff74c35ee7f
SAURABH SINGH <saurabh.s@project42labs.com>
2021-09-20 09:28:55 +0530

    updated url

--- controller/sign.controller.js
+++ controller/sign.controller.js
@@ -1,85 +1,116 @@
 const db = require("../models");
-
+// Import Db object
 const User = db.user;
 const Tag = db.tag;
 const Sequelize = require("sequelize");
 
+// For Create User
 exports.createuser = async (req, res) => {
   try {
     let result = await User.create({ user_name: req.body.user_name });
-    res.send(result);
+    res
+      .status(200)
+      .send({ message: "User created Successfully", result: result });
   } catch (error) {
-    res.send(error.message);
+    res.status(400).send({ message: "Error Occoured", error: error.message });
   }
 };
 
+// For Get All Tags
 exports.gettag = async (req, res) => {
   try {
     let result = await Tag.findAll({
-      order: [["tag_name", "asc"]],
       attributes: ["tag_name"],
     });
-    res.send(result);
+    res
+      .status(200)
+      .send({ message: "Successfully Fetched Tags", data: result });
   } catch (error) {
-    res.send(error.message);
+    res.status(400).send({ message: "Error Occoured", error: error.message });
   }
 };
 
+// Filter Tag on Base of Keyword
 exports.filtertag = async (req, res) => {
   try {
-    let result = await Tag.findAll({
-      where: { tag_name: { [Sequelize.Op.like]: `%${req.query.search}%` } },
-      order: [["tag_name", "asc"]],
-      attributes: ["tag_name"],
-    });
-    res.send(result);
+    if (req.query.search) {
+      let result = await Tag.findAll({
+        where: { tag_name: { [Sequelize.Op.like]: `%${req.query.search}%` } },
+        order: [["tag_name", "asc"]],
+        attributes: ["tag_name"],
+      });
+      res.status(200).send({
+        message: `Successfully Fetched by filter Keyword ${req.params.sarch}`,
+        data: result,
+      });
+    } else {
+      let result = await Tag.findAll({
+        order: [["tag_name", "asc"]],
+        attributes: ["tag_name"],
+      });
+      res.status(200).send({
+        message: `Successfully Fetched by filter Keyword ${req.params.sarch}`,
+        data: result,
+      });
+    }
   } catch (error) {
-    res.send(error.message);
+    res.status(400).send({ message: "Error Occoured", error: error.message });
   }
 };
 
+// Users Can Get all tag created by them
 exports.mytag = async (req, res) => {
   try {
     let result = await Tag.findAll({
-      where: { user_id: req.query.id },
+      where: { user_id: req.params.id },
     });
-    res.send(result);
+    res.status(200).send({
+      message: `Sucessfully Fetched Tags created by user ${req.params.id}`,
+      data: result,
+    });
   } catch (error) {
-    res.send(error.message);
+    res.status(400).send({ message: "Error Occoured", error: error.message });
   }
 };
 
+// User can create tag
 exports.createtag = async (req, res) => {
   try {
     let result = await Tag.create({
       user_id: req.body.id,
       tag_name: req.body.tag,
     });
-    res.send(result);
+    res
+      .status(200)
+      .send({ message: "Succesfully Tag Created", result: result });
   } catch (error) {
-    res.send(error.message);
+    res.status(400).send({ message: "Error Occoured", error: error.message });
   }
 };
 
+// User can Delete Tag Created By them
 exports.deletetag = async (req, res) => {
   try {
     let result = await Tag.destroy({
-      where: { user_id: req.query.id, tag_id: req.query.tagId },
+      where: { user_id: req.params.id, tag_id: req.params.tagId },
     });
-    res.send(`${result}`);
+    res
+      .status(200)
+      .send({ message: `Successfully No of Deleted Tags ${result}` });
   } catch (error) {
-    res.send(error.message);
+    res.status(400).send({ message: "Error Occoured", error: error.message });
   }
 };
 
+// Use Can update tags created by him
 exports.updatetag = async (req, res) => {
   try {
     let result = await Tag.update(
       { tag_name: req.body.tagName },
       { where: { user_id: req.body.id, tag_id: req.body.tagId } }
     );
-    res.send(result);
+    res.status(200).send({ message: `No of Updated Tag ${result}` });
   } catch (error) {
-    res.send(error.message);
+    res.status(400).send({ message: "Error Occoured", error: error.message });
   }
 };
--- router/user_route.js
+++ router/user_route.js
@@ -2,12 +2,25 @@ const express = require("express");
 const router = express.Router();
 const user = require("../controller/sign.controller");
 
+// Create User Router
 router.post("/createuser", user.createuser);
+
+// Create Tag Router
 router.post("/createtag", user.createtag);
+
+// Get all Tag
 router.get("/gettag", user.gettag);
-router.get("/mytag", user.mytag);
+
+// User can get all tag created by him
+router.get("/mytag/:id", user.mytag);
+
+// Filter Tag by keyword or alphabetical order
 router.get("/filtertag", user.filtertag);
+
+// User can updated tag created by him
 router.post("/updatetag", user.updatetag);
-router.delete("/deletetag", user.deletetag);
+
+// User Can Delete Tag Created By Him
+router.delete("/deletetag/:id/:tagId", user.deletetag);
 
 module.exports = router;
